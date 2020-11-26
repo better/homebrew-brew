74aaa057901610980a010ecca75e796f6c4cdc52
Ben Connito <bconnito@better.com>
2020-11-26 11:46:22 -0500

    remove autocomplete stuff

--- Formula/plutus.rb
+++ Formula/plutus.rb
@@ -8,22 +8,10 @@ class Plutus < Formula
     inreplace "bin/plutus", /^CLIENT_HOME=/, "export PLUTUS_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/plutus"
-
-    bash_completion.install "#{libexec}/node_modules/@plutus-cli/plugin-autocomplete/autocomplete/brew/bash"
-    zsh_completion.install "#{libexec}/node_modules/@plutus-cli/plugin-autocomplete/autocomplete/brew/zsh/_plutus"
   end
 
   def caveats; <<~EOS
     To use the Plutus CLI's autocomplete --
-      Via homebrew's shell completion:
-        1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
-            NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
-                  and called, either explicitly or via a framework like oh-my-zsh.
-        2) Then run
-          $ plutus autocomplete --refresh-cache
-
-      OR
-
       Use our standalone setup:
         1) Run and follow the install steps:
           $ plutus autocomplete
