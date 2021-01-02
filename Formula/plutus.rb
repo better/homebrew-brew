cb1750528ca46e683ebce173a73b384c7987335d
Ben Connito <bconnito@better.com>
2021-01-02 08:32:08 -0500

    bump version

--- Formula/plutus.rb
+++ Formula/plutus.rb
@@ -1,16 +1,16 @@
 class Plutus < Formula
   desc "plutus-cli"
   homepage "https://better.com"
-  version "0.29.0"
+  version "0.30.1"
 
   on_macos do
     url "https://plutus-cli.s3.amazonaws.com/plutus-v#{version}/plutus-v#{version}-darwin-x64.tar.gz"
-    sha256 "2b87eca3b9d9107b34ec8a0e744f93476f2d53b417e8212dc8cbf7abfb23f8f1"
+    sha256 "0c18260399b2890624467e015d0a45d7f4c65b0c4576d93430ed9caccc62d783"
   end
   
   on_linux do
     url "https://plutus-cli.s3.amazonaws.com/plutus-v#{version}/plutus-v#{version}-linux-x64.tar.gz"
-    sha256 "674449f2d1b7c73f634ffe9f82229bb143a3412187e4f6944d539cf2217b572e"
+    sha256 "060488f88c828700b457ab4712ccb3c1b4f8cca3e08fd776afdfd3248e379407"
   end
 
   def install
