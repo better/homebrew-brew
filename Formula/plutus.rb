2166e9343c4289a37307e67500026383cb3b8794
Ben Connito <bconnito@better.com>
2021-04-18 08:05:59 -0400

    auto update 0.41.1

--- Formula/plutus.rb
+++ Formula/plutus.rb
@@ -1,16 +1,16 @@
 class Plutus < Formula
   desc "plutus-cli"
   homepage "https://better.com"
-  version "0.40.0"
+  version "0.41.1"
 
   on_macos do
     url "https://plutus-cli.s3.amazonaws.com/plutus-v#{version}/plutus-v#{version}-darwin-x64.tar.gz"
-    sha256 "032776c9cbbf94cc3379b889d2d6c9623c6a2cb98f4999ed35ad040fa576e185"
+    sha256 "ecd5a7591831a112eb08aead83246edeaaccdc876dd50399653df2a863c24b57"
   end
   
   on_linux do
     url "https://plutus-cli.s3.amazonaws.com/plutus-v#{version}/plutus-v#{version}-linux-x64.tar.gz"
-    sha256 "03486efc5e16539957fa3dfccd0810f6027809b7a2505e8dfd5999079d88caed"
+    sha256 "d0e138b9478aa7dda0de1d1be92897a56d8bf780992ed4639a9384f163c01bf7"
   end
 
   def install
