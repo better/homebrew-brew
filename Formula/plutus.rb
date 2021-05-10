4107d42e1c436a3a3cc9a170897216a229ec465f
Better Robot <accounts@better.com>
2021-05-10 15:13:14 +0000

    auto update plutus 0.46.0 [skip ci]

--- Formula/plutus.rb
+++ Formula/plutus.rb
@@ -1,17 +1,17 @@
 class Plutus < Formula
   desc "plutus-cli from binaries"
   homepage "https://better.com"
-  version "0.45.3"
+  version "0.46.0"
 
   on_macos do
-    url "https://plutus-cli.s3.amazonaws.com/plutus-v0.45.3/bin/darwin-x64", :using => :nounzip
-    sha256 "ab1e8fd708c03cef332b20d70a0407e5ed014be7404a4b21e5e3b08dfef937da"
+    url "https://plutus-cli.s3.amazonaws.com/plutus-v0.46.0/bin/darwin-x64", :using => :nounzip
+    sha256 "c832cc6733f564bd110c70706e30293a00b7b4bdea7333eaa8f1859546993fe2"
     $executable_name = "darwin-x64"
   end
   
   on_linux do
-    url "https://plutus-cli.s3.amazonaws.com/plutus-v0.45.3/bin/linux-x64", :using => :nounzip
-    sha256 "fdc9a8cc7f844af2b650de1623e5f5cc22716aad8992db6990076dfa5456f82a"
+    url "https://plutus-cli.s3.amazonaws.com/plutus-v0.46.0/bin/linux-x64", :using => :nounzip
+    sha256 "835661efc6bc2aaff548a825b16d4021a74949a17eef959179ab846287b147cd"
     $executable_name = "linux-x64"
   end
 
