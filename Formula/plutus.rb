fbf42a0379b96ee87d932aeac71e029ae605bdf2
Better Robot <accounts@better.com>
2021-09-07 20:18:18 +0000

    auto update plutus 1.7.2 [skip ci]

--- Formula/plutus.rb
+++ Formula/plutus.rb
@@ -1,16 +1,16 @@
 class Plutus < Formula
   desc "plutus-cli from source"
   homepage "https://better.com"
-  version "1.7.1"
+  version "1.7.2"
 
   on_macos do
     url "https://plutus-cli.s3.amazonaws.com/plutus-v#{version}/plutus-v#{version}-darwin-x64.tar.gz"
-    sha256 "8a11d5a0c79e38863d70368961156d6b1916b8d0eaba01b62da24ba7d87f5c85"
+    sha256 "bd881cf0cfed2f2d14a5420732019dc6e7bba1b8f9575de10b090640e2b32530"
   end
   
   on_linux do
     url "https://plutus-cli.s3.amazonaws.com/plutus-v#{version}/plutus-v#{version}-linux-x64.tar.gz"
-    sha256 "9625b99784e72ece74c639b05442487fdf9c126ff74f77ec45b96e3c31c7ff84"
+    sha256 "44b6008a605a41e5a04f218947d487d239eadcda59165742357ddddf8d597a46"
   end
 
   def install
