ee5bee892652696c721ccc8148d9ce9709f53e2f
Ben Connito <bconnito@better.com>
2025-09-09 15:26:05 -0400

    feat(plutus): add dependencies to formula and template
    add coreutils, awscli, jq, and npm dependencies to the
    plutus formula and template to ensure necessary tools are
    available during installation and runtime
    
    https://bettermortgage.atlassian.net/browse/CORE-280

--- Formula/plutus.rb
+++ Formula/plutus.rb
@@ -7,11 +7,16 @@ class Plutus < Formula
     url "https://plutus-cli.s3.amazonaws.com/plutus-v#{version}/plutus-v#{version}-darwin-x64.tar.gz"
     sha256 "c75c78c3dd0f44c294879af594d91309c00602a40f805c19b00910c4b16a072c"
   end
-  
+
   on_linux do
     url "https://plutus-cli.s3.amazonaws.com/plutus-v#{version}/plutus-v#{version}-linux-x64.tar.gz"
     sha256 "aa0b234ba33ea7ad023d00a1431f3f568a3cd8c259498e0c31210f72ec6255bd"
   end
+
+  depends_on "coreutils"
+  depends_on "awscli"
+  depends_on "jq"
+  depends_on "npm"
 
   def install
     inreplace "bin/plutus", /^CLIENT_HOME=/, "export PLUTUS_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
--- templates/plutus.tpl
+++ templates/plutus.tpl
@@ -7,11 +7,16 @@ class Plutus < Formula
     url "https://plutus-cli.s3.amazonaws.com/plutus-v#{version}/plutus-v#{version}-darwin-x64.tar.gz"
     sha256 "$MACOS_GZ_SHA256"
   end
-  
+
   on_linux do
     url "https://plutus-cli.s3.amazonaws.com/plutus-v#{version}/plutus-v#{version}-linux-x64.tar.gz"
     sha256 "$LINUX_GZ_SHA256"
   end
+
+  depends_on "coreutils"
+  depends_on "awscli"
+  depends_on "jq"
+  depends_on "npm"
 
   def install
     inreplace "bin/plutus", /^CLIENT_HOME=/, "export PLUTUS_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
